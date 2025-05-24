bcacd0f35af32f41ddbbf65ddd10471c99d7fe06
efibalogh <efibalogh@icloud.com>
2025-05-24 03:44:57 +0300

    Fix linter error

--- src/test/java/com/accesa/pricecomparator/PriceComparatorApplicationTests.java
+++ src/test/java/com/accesa/pricecomparator/PriceComparatorApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-class PriceEntryComparatorApplicationTests {
+class PriceComparatorApplicationTests {
 
     // dummy test for now
     @Test
